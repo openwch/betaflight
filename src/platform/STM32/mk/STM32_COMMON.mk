6bcc4ccb063c4019bf253f82a65280de1bbc48a2
mjs1441 <matthew_selby@yahoo.co.uk>
2025-06-10 17:19:11 +0100

    Pico rp2350updates for I2C (#14440)

--- src/platform/STM32/mk/STM32_COMMON.mk
+++ src/platform/STM32/mk/STM32_COMMON.mk
@@ -5,10 +5,8 @@ MCU_COMMON_SRC += \
             common/stm32/system.c \
             common/stm32/config_flash.c \
             common/stm32/bus_spi_pinconfig.c \
-            common/stm32/mco.c \
             drivers/bus_spi_config.c \
-            drivers/serial_pinconfig.c \
-            common/stm32/bus_i2c_pinconfig.c \
+            common/stm32/bus_i2c_config.c \
             common/stm32/bus_spi_hw.c \
             common/stm32/io_impl.c \
             common/stm32/serial_uart_hw.c \
@@ -19,8 +17,7 @@ MCU_COMMON_SRC += \
 
 SIZE_OPTIMISED_SRC += \
             drivers/bus_spi_config.c \
-            drivers/serial_pinconfig.c \
-            common/stm32/bus_i2c_pinconfig.c \
+            common/stm32/bus_i2c_config.c \
             common/stm32/config_flash.c \
             common/stm32/bus_spi_pinconfig.c \
             common/stm32/pwm_output_beeper.c \
