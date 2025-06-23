46c8e7c96469de82318f23a4be8f1eb2b5a91c34
Jay Blackman <blckmn@users.noreply.github.com>
2025-06-23 10:07:49 +1000

    REFACTOR: More platform specific defines moved to platform (#14471)

--- src/main/cli/cli.c
+++ src/main/cli/cli.c
@@ -6598,13 +6598,9 @@ dmaoptEntry_t dmaoptEntryTable[] = {
 #define DMA_OPT_UI_INDEX(i) ((i) + 1)
 #define DMA_OPT_STRING_BUFSIZE 5
 
-<<<<<<< HEAD
-#if defined(STM32H7) || defined(STM32G4) || defined(AT32F435) || defined(CH32H415) 
+#if defined(STM32H7) || defined(STM32G4) || defined(AT32F435)
 #define DMA_CHANREQ_STRING "Request"
 #else
-=======
-#if !defined(DMA_CHANREQ_STRING)
->>>>>>> f45c5c535 (REFACTOR: More platform specific defines moved to platform (#14471))
 #define DMA_CHANREQ_STRING "Channel"
 #endif
 
