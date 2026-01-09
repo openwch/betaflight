9e5344e8ac585cb98e646ede54fcfb31ab944e83
blckmn <blckmn@users.noreply.github.com>
2026-01-09 13:45:13 +1100

    REFACTOR: Camera Control in preparation for RP2350 implementation (#14856)

--- src/platform/STM32/mk/STM32_COMMON.mk
+++ src/platform/STM32/mk/STM32_COMMON.mk
@@ -8,6 +8,7 @@ MCU_COMMON_SRC += \
             drivers/bus_spi_config.c \
             common/stm32/bus_i2c_config.c \
             common/stm32/bus_spi_hw.c \
+            common/stm32/camera_control.c \
             common/stm32/io_impl.c \
             common/stm32/serial_uart_hw.c \
             common/stm32/dshot_dpwm.c \
