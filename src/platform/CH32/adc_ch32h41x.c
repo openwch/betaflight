973ec764480841881553be0ceef0ca1c1b956f27
x4FF3 <mail@affige.info>
2026-02-10 23:19:14 +0100

    target based cross-compiler

--- src/platform/CH32/adc_ch32h41x.c
+++ src/platform/CH32/adc_ch32h41x.c
@@ -45,7 +45,10 @@
 #include "platform/dma.h"
 
 #include <math.h>
+<<<<<<< HEAD
 
+=======
+>>>>>>> fd9c9cdce (target based cross-compiler)
 const adcDevice_t adcHardware[ADCDEV_COUNT] = {{.ADCx = ADC1,
                                                 .rccADC = RCC_HB2(ADC1),
 #if !defined(USE_DMA_SPEC)
@@ -84,6 +87,7 @@ const adcTagMap_t adcTagMap[] = {
     {DEFIO_TAG_E__PB1, ADC_DEVICES_12, ADC_Channel_9},
 };
 
+<<<<<<< HEAD
 static volatile DMA_DATA uint32_t adcConversionBuffer[ADC_SOURCE_COUNT]; //ADC_SOURCE_COUNT ADC_CHANNEL_COUNT
 
 static void adcInitDevice(const adcDevice_t *adcdev, int channelCount) {
@@ -97,6 +101,21 @@ static void adcInitDevice(const adcDevice_t *adcdev, int channelCount) {
   ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
   ADC_InitStructure.ADC_NbrOfChannel = channelCount;
 
+=======
+static volatile DMA_DATA uint32_t adcConversionBuffer[ADC_CHANNEL_COUNT];
+
+static void adcInitDevice(const adcDevice_t *adcdev, int channelCount) {
+  ADC_TypeDef *Instance = adcdev->ADCx;
+  ADC_InitTypeDef ADC_InitStructure = {0};
+
+  ADC_DeInit(Instance);
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = channelCount;
+
+>>>>>>> fd9c9cdce (target based cross-compiler)
   // Multiple injected channel seems to require scan conversion mode to be
   // enabled even if main (non-injected) channel count is 1.
 #ifdef USE_ADC_INTERNAL
@@ -174,85 +193,167 @@ void adcInit(const adcConfig_t *config) {
 
   // loop over all possible channels and build the adcOperatingConfig to
   // represent the set of enabled channels
-  for (int i = 0; i < ADC_SOURCE_COUNT; i++) {
+  for (int i = 0; i < ADC_CHANNEL_COUNT; i++) {
     int map;
     int dev;
 
-    switch (i){
-      #ifdef USE_ADC_INTERNAL
-        case ADC_TEMPSENSOR:
-          map = ADC_TAG_MAP_TEMPSENSOR;
-          dev = ADCDEV_1;
-          break;
-        case ADC_VREFINT:
-          map = ADC_TAG_MAP_VREFINT;
-          dev = ADCDEV_1;
-          break;
-      #endif
+    if (i == ADC_TEMPSENSOR) {
+      map = ADC_TAG_MAP_TEMPSENSOR;
+      dev = ADCDEV_1;
+    } else if (i == ADC_VREFINT) {
+      map = ADC_TAG_MAP_VREFINT;
+      dev = ADCDEV_1;
+    } else {
+      if (!adcOperatingConfig[i].tag) {
+        continue;
+      }
 
-      default:
-        if (!adcOperatingConfig[i].tag) {
-                continue;
-          }
+      map = adcFindTagMapEntry(adcOperatingConfig[i].tag);
+      if (map < 0) {
+        continue;
+      }
 
-        map = adcFindTagMapEntry(adcOperatingConfig[i].tag);
-        if (map < 0) {
-          continue;
-        }
       // Since ADC1 can do all channels this will only ever return adc1 and is
       // unnecessary
 
-        adcOperatingConfig[i].adcDevice = dev;
-        adcOperatingConfig[i].adcChannel = adcTagMap[map].channel;
-        adcOperatingConfig[i].sampleTime = ADC_SampleTime_CyclesMode7;
-        adcOperatingConfig[i].enabled = true;
+      // Find an ADC instance that can be used for the given TagMap index.
+      // for (dev = 0; dev < ADCDEV_COUNT; dev++) {
+      //     #ifndef USE_DMA_SPEC
+      //     if (!adcDevice[dev].ADCx || !adcDevice[dev].dmaResource) {
+      //         continue;
+      //     }
+      //     #else
+      //     if (!adcDevice[dev].ADCx) {
+      //         continue;
+      //     }
+      //     #endif
 
       //     if (adcTagMap[map].devices & (1 << dev)) {
       //         break;
       //     }
       // }
       dev = ADCDEV_1;
-
     }
-    
 
     adcOperatingConfig[i].adcDevice = dev;
     adcOperatingConfig[i].adcChannel = adcTagMap[map].channel;
     adcOperatingConfig[i].sampleTime = ADC_SampleTime_CyclesMode7;
     adcOperatingConfig[i].enabled = true;
 
-    nChannelsUsed[dev] += 1; // increase the active channel count for this device
+    nChannelsUsed[dev] +=
+        1; // increase the active channel count for this device
 
-        #endif //end of USE_DMA_SPEC
+    // Enable the gpio for analog input
+    if (adcOperatingConfig[i].tag) {
+      IOInit(IOGetByTag(adcOperatingConfig[i].tag), OWNER_ADC_BATT + i, 0);
+      IOConfigGPIO(IOGetByTag(adcOperatingConfig[i].tag),
+                   IO_CONFIG(DIR_IN, GPIO_MODE_IN_AN, GPIO_SPEED_VERY_HIGH,
+                             GPIO_PULL_NONE));
+    }
+  } // for each channel
 
-        // set each channel into the auto sequence for this ADC device
-        for (int adcChan = 0; adcChan < ADC_CHANNEL_COUNT; adcChan++)
-        {
-            // only add enabled channels for the current dev (can be simplified if we drop the pretense at handling adc2 and 3)
-            if (adcOperatingConfig[adcChan].enabled && adcOperatingConfig[adcChan].adcDevice == dev)
-            {
-                adcOperatingConfig[adcChan].dmaIndex = dmaBufferIndex++;
-                ADC_RegularChannelConfig(adc->ADCx,
-                    adcOperatingConfig[adcChan].adcChannel,
-                    adcOperatingConfig[adcChan].dmaIndex+1, // This is the sequence number for the adc conversion
-                    adcOperatingConfig[adcChan].sampleTime );
-            }
-        }
+  // reset all ADC instances，before init we do this;
+  // ADC_DeInit(ADC1);
+  // ADC_DeInit(ADC2);
 
-        adc->ADCx->CTLR1 |= (1<<31);
-        ADC_Cmd(adc->ADCx, ENABLE);
-        ADC_BufferCmd(adc->ADCx, ENABLE);
+  // Common config applies equally to all two ADCs, so only apply it once
+  RCC_ADCCLKConfig(RCC_ADCCLKSource_HCLK);
+  RCC_ADCHCLKCLKAsSourceConfig(RCC_PPRE2_DIV0, RCC_HCLK_ADCPRE_DIV8);
 
-        // ADC_ResetCalibration(adc->ADCx);
-        // while(ADC_GetResetCalibrationStatus(adc->ADCx));
-        // ADC_StartCalibration(adc->ADCx);
-        // while(ADC_GetCalibrationStatus(adc->ADCx));
+  // Enable the clock (power up the circuit)
+  // This has to be done before calling adc_common_config for reasons that are
+  // unclear NB only ADC1 will have any channels, so this loop for pedantry only
+  for (int i = 0; i < ADCDEV_COUNT; i++) {
+    if (nChannelsUsed[i] > 0) {
+      RCC_ClockCmd(adcHardware[0].rccADC, ENABLE);
+    }
+  }
 
-	    // RCC_ADCHCLKCLKAsSourceConfig(RCC_PPRE2_DIV0,RCC_HCLK_ADCPRE_DIV8);
-        ADC_LowPowerModeCmd(adc->ADCx,DISABLE); 
+  // Only adc1 will have any channels assigned to it after the code above, so
+  // this can be simplified
+
+  int dmaBufferIndex = 0;
+  for (int dev = 0; dev < ADCDEV_COUNT; dev++) {
+    const adcDevice_t *adc = &adcHardware[dev];
+
+    // skip this device if it doesn't have any active channels
+    if (nChannelsUsed[dev] == 0) {
+      continue;
+    }
+
+    adcInitDevice(adc, nChannelsUsed[dev]);
+
+#ifdef USE_DMA_SPEC
+
+    // Setup the DMA channel so that data is automatically and continuously
+    // transferred from the ADC output register to the results buffer
+
+    const dmaChannelSpec_t *dmaSpec =
+        dmaGetChannelSpecByPeripheral(DMA_PERIPH_ADC, dev, config->dmaopt[dev]);
+    if (dmaSpec == NULL) {
+      return;
+    }
+
+    dmaIdentifier_e dmaIdentifier = dmaGetIdentifier(dmaSpec->ref);
+    if (!dmaAllocate(dmaIdentifier, OWNER_ADC, RESOURCE_INDEX(dev))) {
+      return;
+    }
+
+    dmaEnable(dmaIdentifier); // enables clock/power
+    xDMA_DeInit(dmaSpec->ref);
+
+    DMA_InitTypeDef DMA_InitStructure = {0};
+
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(adc->ADCx->RDATAR);
+    DMA_InitStructure.DMA_Memory0BaseAddr =
+        (uint32_t)&(adcConversionBuffer[dmaBufferIndex]);
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_BufferSize = nChannelsUsed[dev];
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    xDMA_Init(dmaSpec->ref, &DMA_InitStructure);
+    dmaMuxEnable(dmaIdentifier, dmaSpec->dmaMuxId);
+
+    xDMA_Cmd(dmaSpec->ref, ENABLE); // means dma_channel_enable
+    ADC_DMACmd(adc->ADCx, ENABLE);
+
+#endif // end of USE_DMA_SPEC
+
+    // set each channel into the auto sequence for this ADC device
+    for (int adcChan = 0; adcChan < ADC_CHANNEL_COUNT; adcChan++) {
+      // only add enabled channels for the current dev (can be simplified if we
+      // drop the pretense at handling adc2 and 3)
+      if (adcOperatingConfig[adcChan].enabled &&
+          adcOperatingConfig[adcChan].adcDevice == dev) {
+        adcOperatingConfig[adcChan].dmaIndex = dmaBufferIndex++;
+        ADC_RegularChannelConfig(
+            adc->ADCx, adcOperatingConfig[adcChan].adcChannel,
+            adcOperatingConfig[adcChan].dmaIndex +
+                1, // This is the sequence number for the adc conversion
+            adcOperatingConfig[adcChan].sampleTime);
+      }
+    }
+
+    adc->ADCx->CTLR1 |= (1 << 31);
+    ADC_Cmd(adc->ADCx, ENABLE);
+    ADC_BufferCmd(adc->ADCx, ENABLE);
+
+    // ADC_ResetCalibration(adc->ADCx);
+    // while(ADC_GetResetCalibrationStatus(adc->ADCx));
+    // ADC_StartCalibration(adc->ADCx);
+    // while(ADC_GetCalibrationStatus(adc->ADCx));
+
+    // RCC_ADCHCLKCLKAsSourceConfig(RCC_PPRE2_DIV0,RCC_HCLK_ADCPRE_DIV8);
+    ADC_LowPowerModeCmd(adc->ADCx, DISABLE);
 #ifdef USE_ADC_INTERNAL
-        ADC_TempSensorVrefintCmd(ENABLE);  //only ADC1
+    ADC_TempSensorVrefintCmd(ENABLE); // only ADC1
 #endif
+<<<<<<< HEAD
         ADC_SoftwareStartConvCmd(adc->ADCx, ENABLE);  // start sampling
     }
   } // for each channel
@@ -358,6 +459,8 @@ void adcInit(const adcConfig_t *config) {
 #ifdef USE_ADC_INTERNAL
     ADC_TempSensorVrefintCmd(ENABLE); // only ADC1
 #endif
+=======
+>>>>>>> fd9c9cdce (target based cross-compiler)
     ADC_SoftwareStartConvCmd(adc->ADCx, ENABLE); // start sampling
   }
 }
@@ -366,7 +469,11 @@ void adcInit(const adcConfig_t *config) {
  * Copies the latest ADC external channel data into adcValues defined in adc.c
  */
 void adcGetChannelValues(void) {
+<<<<<<< HEAD
   for (unsigned i = 0; i < ADC_EXTERNAL_COUNT; i++) {
+=======
+  for (int i = 0; i < ADC_CHANNEL_INTERNAL_FIRST_ID; i++) {
+>>>>>>> fd9c9cdce (target based cross-compiler)
     if (adcOperatingConfig[i].enabled) {
       adcValues[adcOperatingConfig[i].dmaIndex] =
           adcConversionBuffer[adcOperatingConfig[i].dmaIndex];
@@ -389,6 +496,7 @@ void adcInternalStartConversion(void) { return; }
 /**
  * Reads a given channel from the DMA buffer
  */
+<<<<<<< HEAD
 uint16_t adcInternalRead(adcSource_e source) {
   // const int dmaIndex = adcOperatingConfig[channel].dmaIndex;
   // return adcConversionBuffer[dmaIndex];
@@ -400,6 +508,11 @@ uint16_t adcInternalRead(adcSource_e source) {
     default:
         return 0;
     }
+=======
+static uint16_t adcInternalRead(int channel) {
+  const int dmaIndex = adcOperatingConfig[channel].dmaIndex;
+  return adcConversionBuffer[dmaIndex];
+>>>>>>> fd9c9cdce (target based cross-compiler)
 }
 
 /**
@@ -414,21 +527,36 @@ uint16_t adcInternalRead(adcSource_e source) {
  * @return the raw ADC reading for the internal voltage reference
  * @see adcInternalCompensateVref in src/main/drivers/adc.c
  */
+<<<<<<< HEAD
 // uint16_t adcInternalReadVrefint(void) {
 //   const uint16_t value = adcInternalRead(ADC_VREFINT);
 
 //   return value;
 // }
+=======
+uint16_t adcInternalReadVrefint(void) {
+  const uint16_t value = adcInternalRead(ADC_VREFINT);
+
+  return value;
+}
+>>>>>>> fd9c9cdce (target based cross-compiler)
 
 /**
  * Read the internal temperature sensor
  *
  * @return the raw ADC reading
  */
+<<<<<<< HEAD
 // uint16_t adcInternalReadTempsensor(void) {
 //   const uint16_t value = adcInternalRead(ADC_TEMPSENSOR);
 //   return value;
 // }
+=======
+uint16_t adcInternalReadTempsensor(void) {
+  const uint16_t value = adcInternalRead(ADC_TEMPSENSOR);
+  return value;
+}
+>>>>>>> fd9c9cdce (target based cross-compiler)
 
 #endif // USE_ADC_INTERNAL
 
--- src/platform/CH32/include/platform/dma.h
+++ src/platform/CH32/include/platform/dma.h
@@ -23,8 +23,8 @@
  */
 #pragma once
 
-#include "platform.h"
 #include "drivers/resource.h"
+#include "platform.h"
 
 #if defined(USE_CHBSP_DRIVER)
 #define PLATFORM_TRAIT_DMA_MUX_REQUIRED 1
@@ -33,75 +33,81 @@
 #define DMA_ARCH_TYPE DMA_Channel_TypeDef
 
 typedef enum {
-    DMA_NONE = 0,
-    DMA1_CH1_HANDLER = 1,
-    DMA1_CH2_HANDLER,
-    DMA1_CH3_HANDLER,
-    DMA1_CH4_HANDLER,
-    DMA1_CH5_HANDLER,
-    DMA1_CH6_HANDLER,
-    DMA1_CH7_HANDLER,
-    DMA1_CH8_HANDLER,
-    DMA2_CH1_HANDLER,
-    DMA2_CH2_HANDLER,
-    DMA2_CH3_HANDLER,
-    DMA2_CH4_HANDLER,
-    DMA2_CH5_HANDLER,
-    DMA2_CH6_HANDLER,
-    DMA2_CH7_HANDLER,
-    DMA2_CH8_HANDLER,
-    DMA_LAST_HANDLER = DMA2_CH8_HANDLER
+  DMA_NONE = 0,
+  DMA1_CH1_HANDLER = 1,
+  DMA1_CH2_HANDLER,
+  DMA1_CH3_HANDLER,
+  DMA1_CH4_HANDLER,
+  DMA1_CH5_HANDLER,
+  DMA1_CH6_HANDLER,
+  DMA1_CH7_HANDLER,
+  DMA1_CH8_HANDLER,
+  DMA2_CH1_HANDLER,
+  DMA2_CH2_HANDLER,
+  DMA2_CH3_HANDLER,
+  DMA2_CH4_HANDLER,
+  DMA2_CH5_HANDLER,
+  DMA2_CH6_HANDLER,
+  DMA2_CH7_HANDLER,
+  DMA2_CH8_HANDLER,
+  DMA_LAST_HANDLER = DMA2_CH8_HANDLER
 } dmaIdentifier_e;
 
-#define DMA_DEVICE_NO(x)    ((((x)-1) / 8) + 1)
-#define DMA_DEVICE_INDEX(x) ((((x)-1) % 8) + 1)
+#define DMA_DEVICE_NO(x) ((((x) - 1) / 8) + 1)
+#define DMA_DEVICE_INDEX(x) ((((x) - 1) % 8) + 1)
 
 uint32_t dmaGetChannel(const uint8_t channel);
 
-#define DMA_OUTPUT_INDEX    0
-#define DMA_OUTPUT_STRING   "DMA%d Channel %d:"
-#define DMA_INPUT_STRING    "DMA%d_CH%d"
+#define DMA_OUTPUT_INDEX 0
+#define DMA_OUTPUT_STRING "DMA%d Channel %d:"
+#define DMA_INPUT_STRING "DMA%d_CH%d"
 
-#define DEFINE_DMA_CHANNEL(d, c, f) { \
-    .dma = d, \
-    .ref = (dmaResource_t *)d ## _Channel ## c ##_BASE, \
-    .irqHandlerCallback = NULL, \
-    .flagsShift = f, \
-    .irqN = d ## _Channel ## c ## _IRQn, \
-    .userParam = 0, \
-    .owner.owner = 0, \
-    .owner.resourceIndex = 0 ,\
-    .dmamux= (dmamux_channel_type *) d ## MUX_CHANNEL ##c \
-    }
+#define DEFINE_DMA_CHANNEL(d, c, f)                                            \
+  {.dma = d,                                                                   \
+   .ref = (dmaResource_t *)d##_Channel##c##_BASE,                              \
+   .irqHandlerCallback = NULL,                                                 \
+   .flagsShift = f,                                                            \
+   .irqN = d##_Channel##c##_IRQn,                                              \
+   .userParam = 0,                                                             \
+   .resourceOwner.owner = 0,                                                   \
+   .resourceOwner.index = 0,                                                   \
+   .dmamux = (dmamux_channel_type *)d##MUX_CHANNEL##c}
 
-#define DMA_HANDLER_CODE  __attribute__((interrupt("WCH-Interrupt-fast")))
+#define DMA_HANDLER_CODE __attribute__((interrupt("WCH-Interrupt-fast")))
 
-#define DEFINE_DMA_IRQ_HANDLER(d, c, i) DMA_HANDLER_CODE void DMA ## d ## _Channel ## c ## _IRQHandler(void) {\
-                                                                        const uint8_t index = DMA_IDENTIFIER_TO_INDEX(i); \
-                                                                        dmaCallbackHandlerFuncPtr handler = dmaDescriptors[index].irqHandlerCallback; \
-                                                                        if (handler) \
-                                                                            handler(&dmaDescriptors[index]); \
-                                                                    }
+#define DEFINE_DMA_IRQ_HANDLER(d, c, i)                                        \
+  DMA_HANDLER_CODE void DMA##d##_Channel##c##_IRQHandler(void) {               \
+    const uint8_t index = DMA_IDENTIFIER_TO_INDEX(i);                          \
+    dmaCallbackHandlerFuncPtr handler =                                        \
+        dmaDescriptors[index].irqHandlerCallback;                              \
+    if (handler)                                                               \
+      handler(&dmaDescriptors[index]);                                         \
+  }
 
 #define DMA_CLEAR_FLAG(d, flag) d->dma->INTFCR = (flag << d->flagsShift)
 #define DMA_GET_FLAG_STATUS(d, flag) (d->dma->INTFR & (flag << d->flagsShift))
-#define DMA_IT_GLOB         ((uint32_t)0x00000001) // channel global interput flag
-#define DMA_IT_TCIF         ((uint32_t)0x00000002) // channel full transport flag
-#define DMA_IT_HTIF         ((uint32_t)0x00000004) // channel half transport flag
-#define DMA_IT_TEIF         ((uint32_t)0x00000008) // channel transport error flag
-
-
+#define DMA_IT_GLOB ((uint32_t)0x00000001) // channel global interput flag
+#define DMA_IT_TCIF ((uint32_t)0x00000002) // channel full transport flag
+#define DMA_IT_HTIF ((uint32_t)0x00000004) // channel half transport flag
+#define DMA_IT_TEIF ((uint32_t)0x00000008) // channel transport error flag
 
 void dmaMuxEnable(dmaIdentifier_e identifier, uint32_t dmaMuxId);
 
-#define xDMA_Init(dmaResource, initStruct)  DMA_Init((DMA_ARCH_TYPE *)(dmaResource), initStruct)
-#define xDMA_DeInit(dmaResource)  DMA_DeInit((DMA_ARCH_TYPE *)(dmaResource))
-#define xDMA_Cmd(dmaResource, newState)  DMA_Cmd((DMA_ARCH_TYPE *)(dmaResource), newState)
-#define xDMA_ITConfig(dmaResource, flags, newState) DMA_ITConfig((DMA_ARCH_TYPE *)(dmaResource), flags, newState)
-#define xDMA_GetCurrDataCounter(dmaResource)  DMA_GetCurrDataCounter((DMA_ARCH_TYPE *)(dmaResource))
-#define xDMA_SetCurrDataCounter(dmaResource, count) DMA_SetCurrDataCounter((DMA_ARCH_TYPE *)(dmaResource), count)
-#define xDMA_GetFlagStatus(dmaResource, flags) DMA_GetFlagStatus((DMA_ARCH_TYPE *)(dmaResource), flags)
-#define xDMA_ClearFlag(dmaResource, flags) DMA_ClearFlag((DMA_ARCH_TYPE *)(dmaResource), flags)
+#define xDMA_Init(dmaResource, initStruct)                                     \
+  DMA_Init((DMA_ARCH_TYPE *)(dmaResource), initStruct)
+#define xDMA_DeInit(dmaResource) DMA_DeInit((DMA_ARCH_TYPE *)(dmaResource))
+#define xDMA_Cmd(dmaResource, newState)                                        \
+  DMA_Cmd((DMA_ARCH_TYPE *)(dmaResource), newState)
+#define xDMA_ITConfig(dmaResource, flags, newState)                            \
+  DMA_ITConfig((DMA_ARCH_TYPE *)(dmaResource), flags, newState)
+#define xDMA_GetCurrDataCounter(dmaResource)                                   \
+  DMA_GetCurrDataCounter((DMA_ARCH_TYPE *)(dmaResource))
+#define xDMA_SetCurrDataCounter(dmaResource, count)                            \
+  DMA_SetCurrDataCounter((DMA_ARCH_TYPE *)(dmaResource), count)
+#define xDMA_GetFlagStatus(dmaResource, flags)                                 \
+  DMA_GetFlagStatus((DMA_ARCH_TYPE *)(dmaResource), flags)
+#define xDMA_ClearFlag(dmaResource, flags)                                     \
+  DMA_ClearFlag((DMA_ARCH_TYPE *)(dmaResource), flags)
 
 #define DMA_CCR_EN 1U
 #define IS_DMA_ENABLED(reg) (((DMA_ARCH_TYPE *)(reg))->CFGR & DMA_CCR_EN)
